3c103ba6269d5f68691759b30cec5d6ae971f9e1
asbelov2 <37810516+asbelov2@users.noreply.github.com>
2021-08-04 16:41:55 +0400

    Результат на 4.08

--- dbscript.sh
+++ dbscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 dbname="postgres"
 dbuser="postgres"
-tables=$(psql -U $dbuser -W -h "localhost" -d $dbname -c "SELECT table_name FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','pg_catalog');")
-echo "$tables"
-for table in $("cat $tables")
-do psql -U $dbuser -W -h "localhost" -d $dbname -c "VACUUM VERBOSE $table; REINDEX TABLE $table"
+PGPASSWORD=12345
+tables=$(psql -U $dbuser -h "localhost" -d $dbname -c "SELECT table_name FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','pg_catalog');")
+echo $tables
+for table in $tables
+do psql -U $dbuser -h "localhost" -d $dbname -c "VACUUM VERBOSE $table; REINDEX TABLE $table"
 done
-read -p -r "Press any key to resume ..."; 
