ebd237c6bb90ce0699662e9f45b345193a52b6ee
asbelov2 <37810516+asbelov2@users.noreply.github.com>
2021-08-05 13:42:10 +0400

    Исправил окончания строк. Теперь скрипт работает.

--- dbscript.sh
+++ dbscript.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-export PGPASSWORD=12345
-user='postgres'
-dbname='postgres'
-tables=$(psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'SELECT table_name FROM information_schema.tables WHERE table_schema = '\'public\''')
-table_arr=($(echo $tables | tr " " "\n"))
-table_arr=("${table_arr[@]:2}")
-unset 'table_arr[-1]'
-unset 'table_arr[-1]'
-for i in "${table_arr[@]}"
-do
-   echo "$i"
-done
-
+export PGPASSWORD=12345;
+user='postgres';
+dbname='postgres';
+tables=$(psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'SELECT table_name FROM information_schema.tables WHERE table_schema = '\'public\''');
+table_arr=($(echo $tables | tr " " "\n"));
+table_arr=("${table_arr[@]:2}");
+unset 'table_arr[-1]';
+unset 'table_arr[-1]';
 for table in "${table_arr[@]}"
-do psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'VACUUM '"$table"';'
-psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'REINDEX TABLE '"$table"';'
+do psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'VACUUM '"$table"';';
+psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'REINDEX TABLE '"$table"';';
 done
