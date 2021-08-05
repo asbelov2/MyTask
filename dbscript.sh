166dddd86e5c037485a15f13a665b1d6286cb8e0
asbelov2 <37810516+asbelov2@users.noreply.github.com>
2021-08-05 11:45:02 +0400

    Работает

--- dbscript.sh
+++ dbscript.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-dbname="postgres"
-dbuser="postgres"
-PGPASSWORD=12345
-tables=$(psql -U $dbuser -h "localhost" -d $dbname -c "SELECT table_name FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','pg_catalog');")
-echo $tables
-for table in $tables
-do psql -U $dbuser -h "localhost" -d $dbname -c "VACUUM VERBOSE $table; REINDEX TABLE $table"
+export PGPASSWORD=12345
+tables=$(psql -b -L 'log.txt' -U 'postgres' -h 'localhost' -d 'postgres' -c 'SELECT table_name FROM information_schema.tables WHERE table_schema = '\'public\''')
+table_arr=($(echo $tables | tr " " "\n"))
+table_arr=("${table_arr[@]:2}")
+unset 'table_arr[-1]'
+unset 'table_arr[-1]'
+for i in "${table_arr[@]}"
+do
+   echo "$i"
+done
+
+for table in "${table_arr[@]}"
+do psql -b -L 'log.txt' -h 'localhost' -U 'postgres' -d 'postgres' -c 'VACUUM '"$table"';'
+psql -b -L 'log.txt' -h 'localhost' -U 'postgres' -d 'postgres' -c 'REINDEX TABLE '"$table"';'
 done
