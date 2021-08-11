321e64b4f78ddee3840c07019d188a50e17169a0
asbelov2 <37810516+asbelov2@users.noreply.github.com>
2021-08-11 13:17:59 +0400

    Добавил резервное копирование посредством pg_dump

--- dbscript.sh
+++ dbscript.sh
@@ -2,6 +2,7 @@
 export PGPASSWORD=12345;
 user='postgres';
 dbname='postgres';
+dumpPath='';
 tables=$(psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'SELECT table_name FROM information_schema.tables WHERE table_schema = '\'public\''');
 table_arr=($(echo $tables | tr " " "\n"));
 table_arr=("${table_arr[@]:2}");
@@ -11,3 +12,5 @@ for table in "${table_arr[@]}"
 do psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'VACUUM '"$table"';';
 psql -b -L 'log.txt' -U "$user" -h 'localhost' -d "$dbname" -c 'REINDEX TABLE '"$table"';';
 done
+pg_dump -d "$dbname" -U "$user" -h 'localhost' > pgsql_$(date "+%Y-%m-%d").dump
+unset PGPASSWORD
